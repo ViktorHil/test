00ca65244a1d4012e748c8e72778d45731f8a3c8
Viktor Hil <VHil@luxoft.com>
2019-10-03 15:23:41 +0300

    PeakCan picture

---
+++ Test2/Inc/debug_comm.h
@@ -0,0 +1,13 @@
+/*
+ * debug_comm.h
+ *
+ *  Created on: Aug 6, 2019
+ *      Author: V
+ */
+
+#ifndef DEBUG_COMM_H_
+#define DEBUG_COMM_H_
+
+void log_comm( const char* info);
+
+#endif /* DEBUG_COMM_H_ */
